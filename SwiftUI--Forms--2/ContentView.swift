5bf34a06a0d158f61afa9d75d2a75b98f70bd908
Swapnil Magar <magarswapnil896@gmail.com>
2020-10-06 14:34:08 +0530

    Added @State property wrapper for two-way binding on textfield

--- SwiftUI--Forms--2/ContentView.swift
+++ SwiftUI--Forms--2/ContentView.swift
@@ -9,8 +9,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var mobile = ""
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    (
+                        Text("First Name")
+                            .bold()
+                            .font(.system(size: 22.0))
+                            .foregroundColor(.black) +
+                        Text(" *")
+                            .bold()
+                            .foregroundColor(.red)
+                    )
+                    TextField("Enter your first name", text: $firstName)
+                }
+                Section {
+                    (
+                        Text("Last Name")
+                            .bold()
+                            .font(.system(size: 22.0))
+                            .foregroundColor(.black) +
+                        Text(" *")
+                            .bold()
+                            .foregroundColor(.red)
+                    )
+                    TextField("Enter your last name", text: $lastName)
+                }
+                Section {
+                    Text("Email ID")
+                        .bold()
+                        .font(.system(size: 22.0))
+                    TextField("Enter your Email ID", text: $email)
+                        .keyboardType(.emailAddress)
+                }
+                Section {
+                    Text("Mobile Number")
+                        .bold()
+                        .font(.system(size: 22.0))
+                    TextField("Enter your mobile number", text: $mobile)
+                        .keyboardType(.phonePad)
+                }
+                Section {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("Button action")
+                        }) {
+                            Text("SUBMIT").bold()
+                                .padding().overlay(
+                                    Capsule(style: .continuous).stroke(Color.black, lineWidth: 2)
+                            )
+                        }
+                        .font(.system(size: 22.0))
+                        .foregroundColor(.black)
+                        .padding()
+                        Spacer()
+                    }
+                }
+            }
+        .navigationBarTitle("Personal Info")
+        }
     }
 }
 
